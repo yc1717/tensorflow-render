abd6c58ce076f5a181731752cc51c27bc5ee7a0d
yc1717 <58168520+yc1717@users.noreply.github.com>
2022-04-27 19:52:59 +0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -92,15 +92,15 @@ async def analyze(request):
     predictions = learn.predict(img)  
     prediction = predictions.argmax()
     if prediction==0:
-        result:'金桔'
+        result='金桔'
     elif prediction==1:
-         result:'檸檬'
+         result='檸檬'
     elif prediction==2:
-        result:'葡萄柚'
+        result='葡萄柚'
     elif prediction==3:
-         result:'柑'
+         result='柑'
     else:
-        result:'柳丁'
+        result='柳丁'
     return JSONResponse({'result': str(result)})
 
 
